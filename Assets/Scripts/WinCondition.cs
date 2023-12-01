839441f87b0e57806249b72a9db75ab60b1e8b77
lulolalo-jpg <nscampagna@gmail.com>
2023-12-01 10:20:32 -0300

    score

---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinCondition : MonoBehaviour
+{ 
+
+    public static WinCondition instance;
+
+    public int enemigosEliminados = 0;
+    public int objetivoEnemigos = 50;
+
+    public Text textoGanador;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        ActualizarTexto();
+    }
+
+    public void EnemigoEliminado()
+    {
+        enemigosEliminados++;
+
+        ActualizarTexto();
+
+        if (enemigosEliminados >= objetivoEnemigos)
+        {
+            GanarJuego();
+        }
+    }
+
+    void ActualizarTexto()
+    {
+        textoGanador.text = "Enemigos Eliminados: " + enemigosEliminados + " / " + objetivoEnemigos;
+    }
+
+    void GanarJuego()
+    {
+        Debug.Log("¡Has ganado el juego!");
+        // Puedes agregar aquí cualquier acción que desees cuando el jugador gane el juego.
+    }
+}
